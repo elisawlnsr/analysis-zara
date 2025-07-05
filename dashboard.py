9471fabf5ea2ef37c51042c342dad22237631b0c
elisawlns <elisawulansari03@gmail.com>
2025-07-05 12:36:18 +0700

    update dashboard

--- dashboard.py
+++ dashboard.py
@@ -25,6 +25,8 @@ def show_logo_in_sidebar(image_path, width=250):
             unsafe_allow_html=True
         )
 
+show_logo_in_sidebar("assets/Zara_Logo.svg")
+
 
 # Load Data
 df = pd.read_csv("zara_cleaned.csv")
